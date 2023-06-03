c740dfc9c394adf06890508baa085e0f87beecce
Chai Phonbopit <phonbopit@gmail.com>
2023-06-03 18:45:31 +0700

    update vitest config, use test option in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts,svelte,jsx,tsx}'],
+		globals: true,
+		environment: 'jsdom',
+		coverage: {
+			provider: 'c8'
+		}
 	}
 });
