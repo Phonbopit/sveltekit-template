83532576e30fe5ec53951c15b746c4cdee5720ab
Chai Phonbopit <phonbopit@gmail.com>
2023-06-03 18:54:12 +0700

    format with prettier and setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-screen">
+  <div class="mx-w-4xl m-auto">
+    <h1 class="text-3xl font-bold text-red-600 my-4">Welcome to SvelteKit</h1>
+    <p>
+      Visit <a class="underline text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read
+      the documentation
+    </p>
+  </div>
+</div>
