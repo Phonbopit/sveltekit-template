8a8d826ad4455e9f686f877afa83da793fa1af0f
Chai Phonbopit <phonbopit@gmail.com>
2023-06-03 19:01:44 +0700

    make paragraph center

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex h-screen">
   <div class="mx-w-4xl m-auto prose lg:prose-xl">
     <h1 class="text-6xl font-bold text-red-600">Welcome to SvelteKit</h1>
-    <p>
+    <p class="text-center">
       Visit <a class="underline text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read
       the documentation
     </p>
