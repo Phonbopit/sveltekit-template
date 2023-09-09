59cd984f0a6c7da7f380ba3e3c657a0384e01193
Chai Phonbopit <phonbopit@gmail.com>
2023-09-09 15:57:29 +0700

    chore - add alias path for components folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
+<script>
+  import Heading from '$components/Heading.svelte'
+</script>
+
 <div class="flex h-screen">
   <div class="mx-w-4xl m-auto prose lg:prose-xl">
-    <h1 class="text-6xl font-bold text-red-600">Welcome to SvelteKit</h1>
+    <Heading title="Welcome to SvelteKit" />
     <p class="text-center">
       Visit <a class="underline text-red-400" href="https://kit.svelte.dev">kit.svelte.dev</a> to read
       the documentation
